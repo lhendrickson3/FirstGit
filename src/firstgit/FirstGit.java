3e752659569dd312fbe30924b64705d60b01478e
lhendrickson3@my.wctc.edu <>
2013-01-24 17:17:36 -0600

    added new system out print

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -12,5 +12,7 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Good Bye World");
+        System.out.println("LRH");
     }
 }
